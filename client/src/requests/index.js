71342b6ebdd0a10a63b0ce9a9ea40e17899b6314
saurabhsadhu30 <99708532+saurabhsadhu30@users.noreply.github.com>
2024-02-22 22:17:27 +0530

    Update index.js

--- client/src/requests/index.js
+++ client/src/requests/index.js
@@ -2,7 +2,7 @@ import Cookies from "js-cookie";
 
 export const server = location.href.includes("localhost")
   ? "http://127.0.0.1:8080"
-  : "https://quizzie-api-cozd.onrender.com;
+  : "https://quizzie-api-cozd.onrender.com";
 
 export function config() {
   const token = Cookies.get("token");
@@ -12,4 +12,3 @@ export function config() {
     },
   };
 }
-
